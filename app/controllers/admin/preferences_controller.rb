4621255521c4f6b55dbd35cad3b57848c0a6a1bd
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-14 21:44:53 -0400

    commit tree 3461ca4a04d6f408c2da63ff332e6da2778e37a1

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,8 @@
+class Admin::PreferencesController < ApplicationController
+    def index
+        # preferences = Preference.all.first
+        # # byebug
+        # @allow_create_artists = preferences.allow_create_artists
+        # @allow_create_songs = preferences.allow_create_songs
+    end
+end
